51f2a67001613425abe374a522a9d1c4d320a7d4
DanarCahyadi12 <danarcahyadi21@gmail.com>
2023-01-27 23:46:30 -0800

    Update total question setting

---
+++ public/create.js
@@ -0,0 +1,160 @@
+const subjectField = document.querySelector('.subject')
+const descrField = document.querySelector('.description')
+const totalQuestionRadio = document.querySelectorAll('.total-question')
+const questionField = document.querySelector('.question-field')
+const answerField = document.querySelectorAll('.answer-field')
+const detailBtn = document.querySelectorAll('.detail-btn')
+const trueAnswerRadio = document.querySelectorAll('.true-answer')
+const questionContainer = document.querySelector('.container-questions')
+{/* <div class="mt-[1px] py-6  w-full rounded-md p-[20px] shadow-lg bg-white sm:py-12 mb-[200px]">
+<div class="question-container mt-[10px]">
+  <div class="question flex align-center gap-[1px]">
+    <h3 class="number mt-[4px]">1.</h3>
+    <input type="text" class="question-field px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Write question" name="question" required>
+  </div>
+  <div class="answer-container mt-[70px]">
+    <div class="wrapper flex gap-[10px] align-center">
+      <div class="answer flex flex-col gap-[10px]">
+        <div class="answer-wrapper">
+          <label for="answer-a">a.</label>
+          <input type="text" class="answer-field px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-[120px] sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Write answer" name="question" id="answer-a" required>
+        </div>
+        <div class="answer-wrapper">
+          <label for="answer-b">b.</label>
+          <input type="text" class="answer-field px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-[120px] sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Write answer" name="question" id="answer-b" required>
+        </div>
+        <div class="answer-wrapper">
+          <label for="answer-c">c.</label>
+          <input type="text" class="answer-field px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-[120px] sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Write answer" name="question" id="answer-c" required>
+        </div>
+      </div>
+
+      <div class="detail-wrapper flex  gap-[30px]">
+        <div class="wrapper flex flex-col gap-[10px]">
+          <label for="detail-answer" class="detail-btn btn btn-xs mt-[5px]">Detail</label>
+            <label for="detail-answer" class="detail-btn btn btn-xs mt-[15px]">Detail</label>
+            <label for="detail-answer" class="detail-btn btn btn-xs mt-[15px]">Detail</label> 
+        </div>
+        <div class="wrapper-set-true flex flex-col ml-[30px]">
+          <div class="title mt-[-40px]">
+            <h2>Choose true answer</h2>
+          </div>
+          <input type="radio" name="radio-4" class="true-answer radio radio-accent mt-[22px]" value=""/>
+          <input type="radio" name="radio-4" class="true-answer radio radio-accent mt-[23px]" value=""/>
+          <input type="radio" name="radio-4" class="true-answer radio radio-accent mt-[24px]" value=""/>
+        </div>
+      </div>
+    </div>
+    
+  </div>
+</div>
+</div> */}
+const SetTotalQuestion = (num) => {
+    let html = ``
+    for(let i = 1; i <= num; i++){
+        html  += `<div class="mt-[1px] py-6  w-full rounded-md p-[20px] shadow-lg bg-white sm:py-12 mb-[10px]">`
+        html += `<div class="question-container mt-[10px]">
+        <div class="question flex align-center gap-[1px]">
+          <h3 class="number mt-[4px]">${i}.</h3>
+          <input type="text" class="question-field px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Write question" name="question" required>
+        </div>
+        <div class="answer-container mt-[70px]">
+          <div class="wrapper flex gap-[10px] align-center">
+            <div class="answer flex flex-col gap-[10px]">
+              <div class="answer-wrapper">
+                <label for="answer-a">a.</label>
+                <input type="text" class="answer-field px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-[120px] sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Write answer" name="question" id="answer-a" required>
+              </div>
+              <div class="answer-wrapper">
+                <label for="answer-b">b.</label>
+                <input type="text" class="answer-field px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-[120px] sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Write answer" name="question" id="answer-b" required>
+              </div>
+              <div class="answer-wrapper">
+                <label for="answer-c">c.</label>
+                <input type="text" class="answer-field px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-[120px] sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Write answer" name="question" id="answer-c" required>
+              </div>
+            </div>
+      
+            <div class="detail-wrapper flex  gap-[30px]">
+              <div class="wrapper flex flex-col gap-[10px]">
+                <label for="detail-answer" class="detail-btn btn btn-xs mt-[5px]">Detail</label>
+                  <label for="detail-answer" class="detail-btn btn btn-xs mt-[15px]">Detail</label>
+                  <label for="detail-answer" class="detail-btn btn btn-xs mt-[15px]">Detail</label> 
+              </div>
+              <div class="wrapper-set-true flex flex-col ml-[30px]">
+                <div class="title mt-[-40px]">
+                  <h2>Choose true answer</h2>
+                </div>
+                <input type="radio" name="radio-4" class="true-answer radio radio-accent mt-[22px]" value=""/>
+                <input type="radio" name="radio-4" class="true-answer radio radio-accent mt-[23px]" value=""/>
+                <input type="radio" name="radio-4" class="true-answer radio radio-accent mt-[24px]" value=""/>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>`
+        html += `</div>`
+        
+    }   
+    questionContainer.innerHTML = html
+
+}
+const SetDefaultTotalQuestion = () => {
+    let html = ``
+    for(let i = 1; i <= 5; i++){
+        html  += `<div class="mt-[1px] py-6  w-full rounded-md p-[20px] shadow-lg bg-white sm:py-12 mb-[10px]">`
+        html += `<div class="question-container mt-[10px]">
+        <div class="question flex align-center gap-[1px]">
+          <h3 class="number mt-[4px]">${i}.</h3>
+          <input type="text" class="question-field px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-full sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Write question" name="question" required>
+        </div>
+        <div class="answer-container mt-[70px]">
+          <div class="wrapper flex gap-[10px] align-center">
+            <div class="answer flex flex-col gap-[10px]">
+              <div class="answer-wrapper">
+                <label for="answer-a">a.</label>
+                <input type="text" class="answer-field px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-[120px] sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Write answer" name="question" id="answer-a" required>
+              </div>
+              <div class="answer-wrapper">
+                <label for="answer-b">b.</label>
+                <input type="text" class="answer-field px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-[120px] sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Write answer" name="question" id="answer-b" required>
+              </div>
+              <div class="answer-wrapper">
+                <label for="answer-c">c.</label>
+                <input type="text" class="answer-field px-4 py-2 border focus:ring-gray-500 focus:border-gray-900 w-[120px] sm:text-sm border-gray-300 rounded-md focus:outline-none text-gray-600" placeholder="Write answer" name="question" id="answer-c" required>
+              </div>
+            </div>
+      
+            <div class="detail-wrapper flex  gap-[30px]">
+              <div class="wrapper flex flex-col gap-[10px]">
+                <label for="detail-answer" class="detail-btn btn btn-xs mt-[5px]">Detail</label>
+                  <label for="detail-answer" class="detail-btn btn btn-xs mt-[15px]">Detail</label>
+                  <label for="detail-answer" class="detail-btn btn btn-xs mt-[15px]">Detail</label> 
+              </div>
+              <div class="wrapper-set-true flex flex-col ml-[30px]">
+                <div class="title mt-[-40px]">
+                  <h2>Choose true answer</h2>
+                </div>
+                <input type="radio" name="radio-4" class="true-answer radio radio-accent mt-[22px]" value=""/>
+                <input type="radio" name="radio-4" class="true-answer radio radio-accent mt-[23px]" value=""/>
+                <input type="radio" name="radio-4" class="true-answer radio radio-accent mt-[24px]" value=""/>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>`
+        html += `</div>`
+    }
+    questionContainer.innerHTML = html
+
+}
+SetDefaultTotalQuestion()
+totalQuestionRadio.forEach(btn => {
+    btn.addEventListener('change',(e) => {
+        let num = parseInt(e.target.defaultValue)
+        SetTotalQuestion(num)
+    
+    })
+})
